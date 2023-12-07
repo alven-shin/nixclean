6400b044d11a8f8944b5a3e59040cd0c3198dadb
Alvin Shen <29553973+asian-dev@users.noreply.github.com>
2023-12-07 11:04:14 -0500

    Delete old build outputs as well

--- nixclean.py
+++ nixclean.py
@@ -1,3 +1,4 @@
+import itertools
 import os
 import re
 import shutil
@@ -8,16 +9,21 @@ DAYS_TO_KEEP = 7
 
 
 def main() -> None:
-    paths = get_direnvs()
-    deleted, kept = delete_old_envs(paths)
+    direnvs, results = get_environments()
+    deleted_binaries, kept_binaries = delete_old_binaries(results)
+    deleted_direnvs, kept_direnvs = delete_old_envs(direnvs)
     print("This will take a long time...")
 
     subprocess.run(
         ["nix-collect-garbage", "--quiet", "--delete-older-than", f"{DAYS_TO_KEEP}d"]
     )
 
-    print("Deleted:\n{}".format("\n".join(deleted)))
-    print("\nKept:\n{}".format("\n".join(kept)))
+    print(
+        "Deleted:\n{}".format(
+            "\n".join(itertools.chain(deleted_direnvs, deleted_binaries))
+        )
+    )
+    print("\nKept:\n{}".format("\n".join(itertools.chain(kept_direnvs, kept_binaries))))
 
 
 def delete_old_envs(direnvs: list[str]) -> tuple[list[str], list[str]]:
@@ -43,19 +49,41 @@ def delete_old_envs(direnvs: list[str]) -> tuple[list[str], list[str]]:
     return deleted, kept
 
 
-def get_direnvs() -> list[str]:
-    pattern = re.compile(r"^(.+\.direnv)/flake-profile.+ -> .+nix-shell-env$")
+def delete_old_binaries(results: list[str]) -> tuple[list[str], list[str]]:
+    now = time.time()
+    deleted = []
+    kept = []
+
+    for result in results:
+        ctime = os.path.getctime(result)
+        if (now - ctime) / (24 * 60 * 60) > DAYS_TO_KEEP:
+            os.unlink(result)
+            deleted.append(result)
+        else:
+            kept.append(result)
+
+    return deleted, kept
+
+
+def get_environments() -> tuple[list[str], list[str]]:
+    direnv_pattern = re.compile(r"^(.+\.direnv)/flake-profile.+ -> .+nix-shell-env$")
+    result_pattern = re.compile(r"^(.+/result) ->")
     process = subprocess.run(
         ["nix-store", "--gc", "--print-roots"], capture_output=True
     )
-    paths = []
+    direnvs = []
+    results = []
 
     for line in process.stdout.decode("utf-8").splitlines():
-        match = pattern.search(line)
-        if match:
-            paths.append(match.group(1))
+        direnv = direnv_pattern.search(line)
+        if direnv:
+            direnvs.append(direnv.group(1))
 
-    return paths
+        result = result_pattern.search(line)
+        if result:
+            results.append(result.group(1))
+
+    return direnvs, results
 
 
 if __name__ == "__main__":
